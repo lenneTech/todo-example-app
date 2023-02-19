99bc561a0f2c729b37ac2ba3bd30ef56e10cf264
Pascal Klesse <pascal.klesse@lenne.tech>
2023-02-19 13:59:59 +0100

    fix: Fix wrong path

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -7,7 +7,7 @@ STACK_NAME=${STACK_NAME?Variable not set} \
 APP_URL=${APP_URL?Variable not set} \
 IMAGE_TAG=${IMAGE_TAG?Variable not set} \
 CI_REGISTRY_IMAGE=${CI_REGISTRY_IMAGE?Variable not set} \
-docker compose \
+docker-compose \
 -f ${FILE_NAME?Variable not set} \
 build
 
@@ -15,6 +15,6 @@ STACK_NAME=${STACK_NAME?Variable not set} \
 APP_URL=${APP_URL?Variable not set} \
 IMAGE_TAG=${IMAGE_TAG?Variable not set} \
 CI_REGISTRY_IMAGE=${CI_REGISTRY_IMAGE?Variable not set} \
-docker compose \
+docker-compose \
 -f ${FILE_NAME?Variable not set} \
 push
